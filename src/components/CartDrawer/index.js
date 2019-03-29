6714b83118d1296e3585c56d5106785c3dfcaba6
Mehul patel <mehulptl93@gmail.com>
2019-03-28 21:00:50 -0700

    Add cart drawer button

--- src/components/CartDrawer/index.js
+++ src/components/CartDrawer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import emptyBag from '../../assets/images/empty-bag.svg';
 
 class CartDrawer extends Component {
@@ -14,6 +15,9 @@ class CartDrawer extends Component {
           <div className="emptycart-text">
            <h3>Your bag is empty.</h3>
           </div>
+        </div>
+        <div className="p-4 border-top position-absolute w-100" style={{ bottom: 0 }}>
+         <Button className="disabled rounded bg-primary w-100 m-0 text-lowercase">Add $12.51 to proceed</Button>
         </div>
       </div>
     );
